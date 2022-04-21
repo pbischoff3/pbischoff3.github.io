588fb83a12f4acb745500c85af9712cdf84b4a7f
pbischoff3 <pbischoff3@gmail.com>
2022-04-21 08:41:31 -0600

    4/21

--- temp.R
+++ temp.R
@@ -14,3 +14,9 @@
 ### *Do you care for someone who has a medical condition?*
 
 ## **Conclusion**
+
+
+##############################################
+<h3 style="background-color:darkgray;text-align:center;font-size:180%;">[Home](https://pbischoff3.github.io/) | [Data Overview](https://pbischoff3.github.io/data_overview/) | [Multi-Dimensional Analysis](https://pbischoff3.github.io/mutli_dimensional_analysis/) | [Conclusion](https://pbischoff3.github.io/Conclusion/)</h3>
+  
+  <h4 style="background-color:cornsilk;text-align:center;font-size:95%;">[Major](https://pbischoff3.github.io/mutli_dimensional_analysis/Major/) | [Gender](https://pbischoff3.github.io/mutli_dimensional_analysis/Gender/) | [Career Goals](https://pbischoff3.github.io/mutli_dimensional_analysis/career/) | [Ethnicity](https://pbischoff3.github.io/mutli_dimensional_analysis/Ethnicity/) | [First Generation Students](https://pbischoff3.github.io/mutli_dimensional_analysis/fgs/) | [Number of Dependents](https://pbischoff3.github.io/mutli_dimensional_analysis/dependents/)      | [Medical Condition](https://pbischoff3.github.io/mutli_dimensional_analysis/medical_condition/) | [Medical Care](https://pbischoff3.github.io/mutli_dimensional_analysis/medical_care/)</h4>
